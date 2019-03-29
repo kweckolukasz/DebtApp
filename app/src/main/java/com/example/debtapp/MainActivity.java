0f05d75c9601eca4b0077d9d25d5943e28935b6b
kweckolukasz <kweckolukasz@gmail.com>
2019-03-29 19:47:57 +0100

    v. 0.2 podstawowy, pojedyńczy ekran działa, dodałem mapy i zapis długów

--- app/src/main/java/com/example/debtapp/MainActivity.java
+++ app/src/main/java/com/example/debtapp/MainActivity.java
@@ -1,25 +1,39 @@
 package com.example.debtapp;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.example.debtapp.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    Context mContext;
 
     Integer amount = 0;
     Integer amountDiff;
     Integer totalAmount;
-
+    List<String> people;
     EditText debt_amount;
+    RadioGroup rg;
+    LinearLayout ll;
+    Map<String, HashMap> globalMap;
 
     private static final String DEBT_AMOUNT_LESSER_THAN_0 = "cannot enter no-positive value, click change direction button to divert the arrow";
 
@@ -27,7 +41,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mContext = getApplicationContext();
+        people = new ArrayList<>();
+        rg = binding.radioLeft;
+        ll = binding.checkboxes;
+        globalMap = new HashMap<>();
+        keepNoMinusValuesInsideDebtAmountText();
 
+
+    }
+
+
+    private void keepNoMinusValuesInsideDebtAmountText() {
         debt_amount = binding.debtAmount;
         debt_amount.addTextChangedListener(new TextWatcher() {
 
@@ -59,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void calculutate(View view) {
+    public void calculate(View view) {
         amount = Integer.valueOf(binding.debtAmount.getText().toString());
         switch (view.getId()) {
             case R.id.add1:
@@ -90,5 +115,67 @@ public class MainActivity extends AppCompatActivity {
         amount = Integer.valueOf(binding.debtAmount.getText().toString());
         totalAmount = amount + amountDiff;
         binding.debtAmount.setText(String.format("%d", totalAmount));
+    }
+
+    public void createNewPerson(View view) {
+        String personInput = binding.newPersonName.getText().toString();
+        boolean isNameNotRepeat = true;
+        if (people != null && people.size() > 0) {
+            for (String checkPerson :
+                    people) {
+                if (checkPerson.equals(personInput)) {
+                    isNameNotRepeat = false;
+                    Toast.makeText(getApplicationContext(), "NAME REPEAT", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+        if (isNameNotRepeat && people != null) {
+
+            HashMap<String, Integer> individualMap = new HashMap<>();
+            globalMap.put(personInput, individualMap);
+
+            people.add(personInput);
+            RadioButton rb = new RadioButton(this);
+            rb.setText(personInput);
+            rb.setId(personInput.hashCode());
+            rg.addView(rb);
+            CheckBox cb = new CheckBox(this);
+            cb.setText(personInput);
+            cb.setId(personInput.hashCode());
+            ll.addView(cb);
+        }
+
+    }
+
+    public void submitDebt(View view) {
+
+        //wyciągam imię pożyczającego
+        int checkedId = binding.radioLeft.getCheckedRadioButtonId();
+        RadioButton checked = rg.findViewById(checkedId);
+        String debtGiver = checked.getText().toString();
+
+        //wyciągam imiona wierzycieli
+        ArrayList<String> receivers = new ArrayList();
+        LinearLayout checkboxes = binding.checkboxes;
+        for (int i = 0; i < checkboxes.getChildCount(); i++) {
+            View potentialCheckbox = checkboxes.getChildAt(i);
+            if (potentialCheckbox instanceof CheckBox) {
+                CheckBox potentialReceiver = (CheckBox) potentialCheckbox;
+                if (potentialReceiver.isChecked()) {
+                    String receiverName = potentialReceiver.getText().toString();
+                    receivers.add(receiverName);
+                }
+            }
+        }
+
+        amount = Integer.valueOf(binding.debtAmount.getText().toString());
+        HashMap debtGiverMap = globalMap.get(debtGiver);
+        for (String debtReceiver :
+                receivers) {
+            debtGiverMap.put(debtReceiver, amount);
+            globalMap.get(debtReceiver).put(debtGiver,amount*(-1));
+        }
+
+
     }
 }
