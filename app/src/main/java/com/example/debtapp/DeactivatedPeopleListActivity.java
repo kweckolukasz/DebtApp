6fa4ec5e0a3bd32119320628905d4cc91888f903
LukaszKwecko <kweckolukasz@gmail.com>
2020-04-10 07:04:58 +0200

    powrót do projektu, musze zapisac postep zeby moc wyslac to na github

---
+++ app/src/main/java/com/example/debtapp/DeactivatedPeopleListActivity.java
@@ -0,0 +1,14 @@
+package com.example.debtapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeactivatedPeopleListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deactivated_people_list);
+    }
+}
